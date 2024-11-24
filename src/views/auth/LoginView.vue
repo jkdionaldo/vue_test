1e7c98d0216f636d36141b2727ff8288c9e42cb4
kev <jankevin.dionaldo64@gmail.com>
2024-11-24 19:18:08 +0800

    feat: addded remember me with animation; added a rule when creating username and password in register form

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -3,14 +3,19 @@
 <script setup>
 import AppLayout from "@/components/layout/AppLayout.vue";
 import { useDisplay } from "vuetify";
+import "@/assets/login_style.css";
 
 const { mobile } = useDisplay();
+
+import { ref } from "vue";
+
+const visible = ref(false);
 </script>
 
 <template>
   <AppLayout>
     <template #content>
-      <v-row>
+      <v-row class="">
         <v-col cols="12" md="6" class="mx-auto" style="margin-top: 10%">
           <v-card elevation="24" class="mx-auto" width="100%">
             <v-card-title class="text-center">
@@ -32,17 +37,24 @@ const { mobile } = useDisplay();
                   label="Username"
                   placeholder="Enter your Username"
                   persistent-clear
+                  prepend-inner-icon="mdi-email-outline"
                 ></v-text-field>
 
                 <v-text-field
-                  clearable
                   label="Password"
-                  placeholder="Enter your Password"
-                  persistent-clear
+                  :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="visible ? 'text' : 'password'"
+                  placeholder="Enter your password"
+                  prepend-inner-icon="mdi-lock-outline"
+                  @click:append-inner="visible = !visible"
                 ></v-text-field>
 
                 <v-class class="d-flex justify-space-between">
-                  <h6>Remember Me</h6>
+                  <label class="checkbox-container">
+                    <input class="custom-checkbox" type="checkbox" />
+                    <span class="checkmark"></span>
+                    <small>Remember Me</small>
+                  </label>
                   <h6 class="text-right">Forgot Password?</h6>
                 </v-class>
 
@@ -53,9 +65,13 @@ const { mobile } = useDisplay();
 
               <v-divider class="my-4"></v-divider>
               <h6 class="my-3 text-center">
-                <RouterLink to="/register" class="text-primary"
-                  >Sign up here</RouterLink
-                >
+                Don't have an account?
+                <RouterLink
+                  to="/register"
+                  class="text-primary"
+                  style="text-decoration: none"
+                  >Sign up here<v-icon icon="mdi-chevron-right"></v-icon
+                ></RouterLink>
               </h6>
             </v-sheet>
           </v-card>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,5 +1,22 @@
 <script setup>
 import AppLayout from "@/components/layout/AppLayout.vue";
+
+//For pass verification
+import { ref } from "vue";
+
+const rules = {
+  required: (value) => !!value || "Required.",
+  min: (v) => v.length >= 8 || "Min 8 characters",
+  emailMatch: () => `The email and password you entered don't match`,
+};
+
+const show1 = ref(false);
+const show2 = ref(true);
+const password = ref("Password");
+
+//for eye
+
+const visible = ref(false);
 </script>
 
 <template>
@@ -14,7 +31,7 @@ import AppLayout from "@/components/layout/AppLayout.vue";
             style="margin-top: 10%"
           >
             <v-card-title class="text-center">
-              <v-image src="/logo.png" width="50"></v-image>
+              <!--<v-image src="/logo.png" width="50"></v-image>-->
               <h4 class="font-weight-black text-center">Cam'n Go</h4>
               <small>Register Form</small>
             </v-card-title>
@@ -40,20 +57,24 @@ import AppLayout from "@/components/layout/AppLayout.vue";
                   label="Username"
                   placeholder="Enter your Username"
                   persistent-clear
+                  :rules="[rules.required, rules.min]"
+                  hint="At least 8 characters"
+                  name="input-10-1"
+                  counter
+                  @click:append="show1 = !show1"
                 ></v-text-field>
 
                 <v-text-field
-                  clearable
                   label="Password"
-                  placeholder="Enter your Password"
-                  persistent-clear
-                ></v-text-field>
-
-                <v-text-field
-                  clearable
-                  label="Confirm Password"
-                  placeholder="Re-Type your Password"
-                  persistent-clear
+                  :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="visible ? 'text' : 'password'"
+                  placeholder="Enter your password"
+                  @click:append-inner="visible = !visible"
+                  :rules="[rules.required, rules.min]"
+                  hint="At least 8 characters"
+                  name="input-10-1"
+                  counter
+                  @click:append="show1 = !show1"
                 ></v-text-field>
 
                 <v-btn
@@ -64,12 +85,14 @@ import AppLayout from "@/components/layout/AppLayout.vue";
                   >Register</v-btn
                 >
 
-                <divider class="my-4"></divider>
                 <h6 class="my-3 text-center">
                   Already have an account?
-                  <RouterLink to="/" class="text-primary"
-                    >Back to Login</RouterLink
-                  >
+                  <RouterLink
+                    to="/"
+                    class="text-primary"
+                    style="text-decoration: none"
+                    >Back to Login<v-icon icon="mdi-chevron-right"></v-icon
+                  ></RouterLink>
                 </h6>
               </v-form>
             </v-sheet>
